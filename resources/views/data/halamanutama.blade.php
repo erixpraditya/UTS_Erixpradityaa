175009cf39adba34b2e982fbe637e615a0bf7bd9
erixpraditya <erixpradityaa@gmail.com>
2025-03-21 15:41:32 +0700

    update_halamanutama

--- resources/views/data/halamanutama.blade.php
+++ resources/views/data/halamanutama.blade.php
@@ -4,9 +4,9 @@
 @endsection
 
 @section('konten')
-<div class="d-flex flex-column justify-content-center align-items-center" style="height: 60vh;">
+<div class="d-flex flex-column justify-content-center align-items-center" style="height: 70vh;">
     <div class="card shadow d-flex flex-column justify-content-center align-items-center text-center"
-        style="width: 300px; height: 300px; border-radius: 50%; padding: 20px;">
+        style="width: 300px; height: 300px; border-radius: 50%; padding: 30px;">
         <i class="fa-solid fa-user text-primary" style="font-size: 150px;"></i>
     </div>
     <h3 class="m-0 font-weight-bold text-primary mt-3">Admin Erix Praditya</h3> <!-- Tambah margin top -->
@@ -15,21 +15,21 @@
 
     <!-- Page Heading -->
     <div class="d-flex justify-content-center align-items-center mb-5" style="height: 30vh; gap: 10px;">
-        <a href="data.tambahbuku">
+        <a href="/tambahbuku">
         <div class="card shadow mb-4 d-flex flex-column justify-content-center align-items-center text-center" style="width: 300px; height: 300px;">
             <i class="fa-solid fa-book text-primary" style="font-size: 40px;"></i>
             <h6 class="m-0 font-weight-bold text-primary mt-2">Tambah Buku</h6>
         </div>
         </a>
 
-        <a href="/tambah">
+        <a href="/tambahpeminjam">
         <div class="card shadow mb-4 d-flex flex-column justify-content-center align-items-center text-center" style="width: 300px; height: 300px;">
             <i class="fa-regular fa-folder-open text-primary" style="font-size: 40px;"></i>
             <h6 class="m-0 font-weight-bold text-primary mt-2">Tambah Peminjam</h6>
         </div>
         </a>
 
-        <a href="data.tambahhibah">
+        <a href="/edithibah">
         <div class="card shadow mb-4 d-flex flex-column justify-content-center align-items-center text-center" style="width: 300px; height: 300px;">
             <i class="fa-regular fa-handshake text-primary" style="font-size: 40px;"></i>
             <h6 class="m-0 font-weight-bold text-primary mt-2">Tambah Hibah</h6>
